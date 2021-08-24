55c019d5619ebe0501e85ffbbfafd72686686ded
三少 <sanshao@dian.so>
2021-08-24 11:29:49 +0800

    test: 测试格式化工具

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  '**/*.{js,jsx,ts,tsx}': ['npx eslint -c eslint.config.js --fix'],
+  '**/*.{js,jsx,ts,tsx}': ['npx eslint -c eslint.config.js --fix', 'git add'],
   '**/*.ts?(x)': () => 'npx tsc -p tsconfig.json --skipLibCheck',
-  '**/*.{css,less}': ['npx stylelint --aei --config stylelint.config.js --fix'],
-  // '*.{js,jsx,ts,tsx,md,html,css,less}': 'npx prettier --write',
+  '**/*.{css,less}': [
+    'npx stylelint --aei --config stylelint.config.js --fix',
+    'git add',
+  ],
+  // '*.{js,jsx,ts,tsx,md,html,css,less}': ['npx prettier --write', 'git add'],
 }
