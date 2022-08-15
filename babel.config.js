822baaa563c8e1b952af0a9e81ff30dada288b3f
三少 <sanshao@dian.so>
2022-08-15 15:38:05 +0800

    fix: 修改配置文件

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,6 @@ module.exports = {
       'taro',
       {
         framework: 'react',
-        'dynamic-import-node': process.env.TARO_ENV === 'h5',
         ts: true,
         useBuiltIns: 'usage',
       },
